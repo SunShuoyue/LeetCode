0c3f30871c8fd3c54370b347d19e40b5727c8fa1
sunshuoyue <shuoyue.sun13@alumni.xjtlu.edu.cn>
2019-04-30 22:17:45 +0800

    array

--- Array/121. Best Time to Buy and Sell Stock.py
+++ Array/121. Best Time to Buy and Sell Stock.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+
         a = 0
         for i in range(len(prices)-1):
             for j in range(i+1,len(prices)):
