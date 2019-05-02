eddb3f4d3430c81869752b57f514eecb114fa723
sunshuoyue <shuoyue.sun13@alumni.xjtlu.edu.cn>
2019-05-02 10:52:00 +0800

    array

--- Array/121. Best Time to Buy and Sell Stock.py
+++ Array/121. Best Time to Buy and Sell Stock.py
@@ -4,11 +4,15 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-
         a = 0
-        for i in range(len(prices)-1):
-            for j in range(i+1,len(prices)):
-                d = prices[j] - prices[i]
+        if len(prices) < 2:
+            return 0
+        ind = [i for i in range(1, len(prices)) if (prices[i] - prices[i - 1]) > 0]
+        for i in range(len(ind)):
+            for j in ind[i:]:
+                d = prices[j] - prices[ind[i] - 1]
                 if d > a:
                     a = d
         return a
+
+# actually this solution is a bit tricky. I wrote a double for loop which chould't pass the last test, so I came out with this idea.
