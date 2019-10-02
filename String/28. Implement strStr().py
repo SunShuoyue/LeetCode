b875b42e59034082e5546cbc25c0997339421ce7
sunshuoyue <shuoyue.sun13@alumni.xjtlu.edu.cn>
2019-10-02 21:00:10 +0800

    string

---
+++ String/28. Implement strStr().py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle in haystack:
+            for i in range(len(haystack)-len(needle)+1):
+                if haystack[i:i+len(needle)] == needle:
+                    return i
+        else:
+            return -1
+
