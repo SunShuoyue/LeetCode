d70e8de4fc2c263c1f85b247cd0610e659e97126
sunshuoyue <shuoyue.sun13@alumni.xjtlu.edu.cn>
2019-10-20 22:51:51 +0800

    tree

--- Tree/102. Binary Tree Level Order Traversal.py
+++ Tree/102. Binary Tree Level Order Traversal.py
@@ -11,8 +11,21 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        l = self.levelOrder(root.left)
-        r = self.levelOrder(root.right)
+        if not root:
+            return []
+        l = []
+        r = []
+        if root.left:
+            l = self.levelOrder(root.left)
+            if not l:
+                l = []
+        if root.right:
+            r = self.levelOrder(root.right)
+            if not r:
+                r = []
         if len(l) >= len(r):
-            res = [l[i]+r[i] for i in range(len(r))] + l[len(r):]
-        return res.insert([root.val])
+            res = [l[i] + r[i] for i in range(len(r))] + l[len(r):]
+        else:
+            res = [l[i] + r[i] for i in range(len(l))] + r[len(l):]
+        res.insert(0, [root.val])
+        return res
