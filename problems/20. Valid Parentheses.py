3ad0a0ed04e3f9d00cd04bc0e70b559db79f38c4
sunshuoyue <shuoyue.sun13@alumni.xjtlu.edu.cn>
2020-04-25 16:48:00 +0800

    problems

---
+++ problems/20. Valid Parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        check = []
+        pairs = {')': '(', '}': '{', ']': '['}
+        for i in s:
+            if i in pairs:
+                if len(check) < 1:
+                    return False
+                if check.pop() != pairs[i]:
+                    return False
+            else:
+                check.append(i)
+        if check == []:
+            return True
+        else:
+            return False
